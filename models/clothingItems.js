b6271e94008b73c433a93b4a1bcaf63219b0ce24
rio <riojordan127@yahoo.com>
2025-08-30 21:59:31 -0500

    add clothing item schema

--- models/clothingItems.js
+++ models/clothingItems.js
@@ -4,16 +4,37 @@ const clothingItemSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
+    minlength: 2,
+    maxlength: 30,
   },
-  email: {
+  weather: {
     type: String,
     required: true,
-    unique: true,
+    enum: ["hot", "warm", "cold"],
   },
-  password: {
+  imageUrl: {
     type: String,
     required: true,
+    validate: {
+      validator() {
+        return validator.isURL(this.imageUrl);
+      },
+      message: 'You must enter a valid URL',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 
