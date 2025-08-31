ec6cd2a0fa68d1419233e2bb76e122d1ce5b0c50
rio <riojordan127@yahoo.com>
2025-08-31 18:39:16 -0500

    set up user controllers

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ const mainRouter = require("./routes/index");
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch(console.error);
 
-  app.use("/", mainRouter);
+app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '68b4dc7f1fea408baada6212'
+  };
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
