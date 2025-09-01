22fcc93f4499438089602cbe1ab3511ba581f6c4
rio <riojordan127@yahoo.com>
2025-09-01 17:46:54 -0500

    polish for fourth submission

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { PORT = 3001 } = process.env;
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
+    // eslint-disable-next-line no-console
     console.log("Connected to MongoDB");
   })
   .catch(console.error);
@@ -24,5 +25,6 @@ app.use((req, res, next) => {
 app.use(routes);
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on port ${PORT}`);
 });
